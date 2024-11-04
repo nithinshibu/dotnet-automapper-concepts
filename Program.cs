b9439987fa861aec40415fd091c730d01960a9d2
Nithin Mathew <nithin.mathew@sysfore.com>
2024-11-04 16:00:56 +0530

    Initial Commit - Demonstrated the traditional way

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapperInDotnet.Models;
+
+class Program
+{
+    static void Main()
+    {
+        //Create and Populate Employee Object (TRADITIONAL WAY)
+
+        //Switch between Git branches for automapper examples , master branch shows the traditional way example
+
+        Employee emp = new Employee()
+        {
+            Name="John",
+            Salary=20000,
+            Address="London",
+            Department="IT"
+        };
+
+        //Mapping Employee Object into the EmployeeDTO object
+      
+        EmployeeDTO empDTO = new EmployeeDTO()
+        {
+            Name = emp.Name,
+            Salary = emp.Salary,
+            Address = emp.Address,
+            Department = emp.Department
+        };
+
+        Console.WriteLine("Name: "+empDTO.Name);
+        Console.ReadLine();
+
+        //The main problem in this approach is that in future if any new properties are added in the main class "Employee" then we need to manually map all those 
+        //properties in to the EmployeeDTO class also which can be a hectic and futile task to do.
+
+        //Another problem is that if we use the mapping in more than one place , then in each of those places we need to repeat this steps.
+
+        //In those case, if we use the automapper then this problem will be solved
+
+    }
+}
